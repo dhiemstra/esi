7436c5cf626d8224d39934fb8cc193532b1e550b
Danny Hiemstra <dannyhiemstra@gmail.com>
2017-10-22 14:48:48 +0200

    Add call info class

--- lib/esi/calls.rb
+++ lib/esi/calls.rb
@@ -1,5 +1,23 @@
 module Esi
   class Calls
+    class Info
+      attr_reader :name, :call
+      delegate :scope, :cache_duration, to: :call
+
+      def initialize(name)
+        @name = name.to_sym
+        @call = Calls.const_get(name.to_s.camelize)
+      end
+
+      def character?
+        name.to_s.starts_with?('character')
+      end
+
+      def corporation?
+        name.to_s.starts_with?('corporat')
+      end
+    end
+
     class Base
       class_attribute :scope
       class_attribute :cache_duration
