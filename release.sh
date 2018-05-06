f16629ce109887c95c949c29f24dc366e3bc4e47
Danny Hiemstra <dannyhiemstra@gmail.com>
2018-05-06 15:43:56 +0200

    Location calls

--- release.sh
+++ release.sh
@@ -3,14 +3,18 @@
 set -e
 
 VERSION="$1"
+VERSIONFILE="./lib/eve_app/version.rb"
 
 if [ -z "$VERSION" ]; then
   echo "Usage: ./release.sh VERSION"
   exit 1
 fi
 
-sed -i "s/VERSION = .*/VERSION = '${VERSION}'/" lib/eve_app/version.rb
-bundle install
+if ! grep -q $VERSION "$VERSIONFILE"; then
+  echo "VERSION is not properly set in '${VERSIONFILE}'"
+  echo "Changing it for you.."
+  sed -i "s/VERSION = .*/VERSION = '${VERSION}'/" "$VERSIONFILE"
+fi
 
 git commit -am "Release v${VERSION}"
 git tag "v${VERSION}" -a -m "Release v${VERSION}"
